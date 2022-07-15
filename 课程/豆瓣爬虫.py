68f054ce211c4a8e0c0863632971c1fc64b75cf3
fatpo <564545913@qq.com>
2022-07-15 20:53:27 +0800

    init

--- 课程/豆瓣爬虫.py
+++ 课程/豆瓣爬虫.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Author: Kun
 import pandas as pd
 import requests
 from lxml import etree
@@ -7,20 +5,21 @@ from lxml import etree
 df = []
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4343.0 Safari/537.36',
-    'Referer': 'https://movie.douban.com/top250'}
+    'Referer': 'https://movie.douban.com/top250'
+}
 columns = ['排名', '电影名称', '导演', '上映年份', '制作国家', '类型', '评分', '评价分数', '短评']
 
 
-def get_data(html):
-    xp = etree.HTML(html)
+def get_data(_html):
+    xp = etree.HTML(_html)
     lis = xp.xpath('//*[@id="content"]/div/div[1]/ol/li')
     for li in lis:
         """排名、标题、导演、演员、"""
         ranks = li.xpath('div/div[1]/em/text()')
         titles = li.xpath('div/div[2]/div[1]/a/span[1]/text()')
         directors = li.xpath('div/div[2]/div[2]/p[1]/text()')[0].strip().replace("\xa0\xa0\xa0", "\t").split("\t")
-        infos = li.xpath('div/div[2]/div[2]/p[1]/text()')[1].strip().replace('\xa0', '').split('/')
-        dates, areas, genres = infos[0], infos[1], infos[2]
+        info_list = li.xpath('div/div[2]/div[2]/p[1]/text()')[1].strip().replace('\xa0', '').split('/')
+        dates, areas, genres = info_list[0], info_list[1], info_list[2]
         ratings = li.xpath('.//div[@class="star"]/span[2]/text()')[0]
         scores = li.xpath('.//div[@class="star"]/span[4]/text()')[0][:-3]
         quotes = li.xpath('.//p[@class="quote"]/span/text()')
